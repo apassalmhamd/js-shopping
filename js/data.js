cc8291fbb3473617679298e4a6d0122b4faa32b5
apassalmhamd <121682976+apassalmhamd@users.noreply.github.com>
2024-04-02 17:24:31 +0300

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,16 @@
+let productsDB=[
+    {
+        id:1,
+        title:"شركة الفندي",
+        title2:"انقر لعرض الصور",
+        desc:'بيع جميع انواع السيارات والصناديق',
+        size:"largee",
+        imageUrl1:"images/img1.jpg",
+        imageUrl2:"images/img1.jpg",
+        imageUrl3:"images/img1.jpg",
+        qty:1,
+        isMe:"N",
+    },
+    
+    
+];
